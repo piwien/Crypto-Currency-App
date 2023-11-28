ff7d0f9003139a81a758ce20ee53eaff46b64452
piwien <berkayyasar01@gmail.com>
2023-11-28 12:47:10 +0300

    Initial Commit

---
+++ CryptoCurrencyApp/CryptoCurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyAppApp.swift
+//  CryptoCurrencyApp
+//
+//  Created by Berkay Yaşar on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
